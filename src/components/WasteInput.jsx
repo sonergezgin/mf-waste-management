e33525faf428dfce15a132017636325b6714d21a
sonergezgin <edirne.soner@gmail.com>
2024-06-07 03:55:14 +0300

    finish waste input submit endpoint

--- src/components/WasteInput.jsx
+++ src/components/WasteInput.jsx
@@ -5,6 +5,8 @@ import useLogout from "../hooks/useLogout";
 import Form from 'react-bootstrap/Form';
 
 import axios from "../api/axios";
+
+
 const WasteInput = () => {
 
     const navigate = useNavigate();
@@ -12,6 +14,9 @@ const WasteInput = () => {
 
 
     const [wasteTypes, setWasteTypes] = useState([]);
+    const [weight, setWeight] = useState(0);
+
+    const [choosenWasteType, setChoosenWasteType] = useState("");
 
     const signOut = async () => {
 
@@ -21,7 +26,7 @@ const WasteInput = () => {
     }
 
     useEffect(() => {
-        fetchWasteTypes()
+        fetchWasteTypes();
     }, [])
 
 
@@ -55,21 +60,70 @@ const WasteInput = () => {
         }
     }
 
-    return (
-        <section>
-            <h1>Waste Input</h1>
-            <br />
+    const handleSubmit = async (e) => {
 
+        e.preventDefault();
 
-            <Form.Select className="wasteInput" aria-label="Default select example">
+        const WASTE_ADD_URL = "api/Waste/add"
 
+        try {
+            const response = await axios.post(WASTE_ADD_URL,
+                JSON.stringify({ wasteTypeId: choosenWasteType, weight: weight }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+
+            console.log("response from submitting waste input", response);
+
+            navigate(from, { replace: true });
+
+        } catch (err) {
+
+            if (!err?.response) {
+                console.error('Server does NOT respond.');
+            } else if (err.response?.status === 401) {
+                console.error('Unauthorized.');
+            } else {
+                console.error('Something went wrong.');
+            }
+        }
+    }
+
+}
+return (
+    <section>
+        <h1>Waste Input</h1>
+        <br />
+
+        <form onSubmit={handleSubmit}>
+
+            <Form.Select
+                className="wasteInput"
+                aria-label="Default select example"
+                onChange={(e) => setChoosenWasteType(e.target.value)}
+                required>
                 <option>Choose the waste type:</option>
                 {wasteTypes.map((wasteType) => (
                     <option value={wasteType.value}>{wasteType.title}</option>
                 ))}
             </Form.Select>
-        </section>
-    )
-}
+
+            <label htmlFor="weight">Weight:</label>
+            <input
+                type="number"
+                id="weight"
+                onChange={(e) => setWeight(e.target.value)}
+                value={weight}
+                required
+            />
+
+            <button>Enter</button>
+
+        </form>
+    </section>
+)
+
 
 export default WasteInput;
